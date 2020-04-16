cd87ee7cd3ccdecb2f7522432823eb778a500b00
Roy Cabouly <htr751@cs.colman.ac.il>
2020-04-16 20:44:44 +0300

    added zip_range, zip function + tested it(all tests passed)

--- MyRangesImplementation/MyRangesImplementation/integreation_tests.cpp
+++ MyRangesImplementation/MyRangesImplementation/integreation_tests.cpp
@@ -8,3 +8,14 @@ TEST_CASE("given vector {1,2,3,4,5} when transforming the vector by multiplying 
 
 	REQUIRE(result == std::vector{ 6, 12 });
 }
+
+TEST_CASE("given vector {1,2,3} and list {roy, tomer, heinan} when ziping the vector and the list, transforming it by concating name and number and filter for strings longer than 4 chars get vector {tomer2, heinan3}", "[integreation_tests]") {
+	auto numbers = std::vector{ 1,2,3 };
+	auto names = std::list<std::string>{ "roy", "tomer", "heinan" };
+
+	std::vector<std::string> result = ranges::view::zip(numbers, names) |
+		ranges::view::transform([](auto&& numberWithName) {return numberWithName.second + std::to_string(numberWithName.first); })
+		| ranges::view::filter([](auto&& name) {return name.length() > 4; });
+
+	REQUIRE(result == std::vector<std::string>{"tomer2", "heinan3"});
+}
